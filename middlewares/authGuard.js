8eb4562f2d39b2a018c475444cb5f8eaaaedb5ec
firas1996 <firas.hm.1996@gmail.com>
2026-03-11 14:28:15 +0100

    aaa

---
+++ middlewares/authGuard.js
@@ -0,0 +1,46 @@
+const { verify } = require("jsonwebtoken");
+const { promisify } = require("util");
+const User = require("../models/userModel");
+
+exports.protectorMW = async (req, res, next) => {
+  try {
+    let token;
+    // 1) thabat si el user 3andou token walé
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+    if (!token) {
+      res.status(401).json({
+        message: "You are not logged in !!!",
+      });
+    }
+    // 2) thabat fel validité mta3 el token
+    const decoded = await promisify(verify)(token, process.env.JWT_SECRET);
+    console.log(decoded);
+    // 3) thabat si el user moula el token mizel mawjoud wala me3edech
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      res.status(401).json({
+        message: "This user no longer exists !!!!",
+      });
+    }
+    // 4) thabat si el token tsan3et 9bal wala ba3d e5er pass update !
+    // console.log(parseInt(user.password_changed_at.getTime() / 1000));
+    // console.log(decoded.iat);
+    if (parseInt(user.password_changed_at.getTime() / 1000) > decoded.iat) {
+      res.status(401).json({
+        message: "You must re-loggin !!!!",
+      });
+    }
+
+    next();
+  } catch (error) {
+    res.status(400).json({
+      message: "Fail !!",
+      error: error,
+    });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,13 +6,14 @@ const {
   updateUserById,
   deleteUserById,
 } = require("../controllers/userController");
+const { protectorMW } = require("../middlewares/authGuard");
 
 const router = require("express").Router();
 
 router.post("/signUp", signUp);
 router.post("/signIn", signIn);
 
-router.route("/users").post(createUser).get(getUsers);
+router.route("/users").post(createUser).get(protectorMW, getUsers);
 
 router
   .route("/users/:id")
