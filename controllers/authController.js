86e6cd0d2ec18f6213821d8d6bc3e9052bae3094
firas1996 <firas.hm.1996@gmail.com>
2026-03-11 12:45:41 +0100

    aa

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,14 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
-const createToken = () => {
-  return jwt.sign({ id, email, role, test: "Hello !" });
+const createToken = (id, email, role) => {
+  return jwt.sign(
+    { id, email, role, test: "Hello !" },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "30d",
+    },
+  );
 };
 
 exports.signUp = async (req, res) => {
@@ -37,8 +43,10 @@ exports.signIn = async (req, res) => {
         message: "Email or password are incorrect !!!",
       });
     }
+    const token = createToken(user._id, user.email, user.role);
     res.status(200).json({
       message: "Logged In !!!!",
+      token,
     });
   } catch (error) {
     res.status(400).json({
